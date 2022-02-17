0b9e458b734e35e8eae78446bbf95f5d69fd678d
Katonic-ML-Marketplace <98009207+Katonic-ML-Marketplace@users.noreply.github.com>
2022-02-17 19:13:52 +0530

    dev push app

--- app/build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t katonic/apps:likelihood-loan-default-prediction .
-docker push katonic/apps:likelihood-loan-default-prediction
-# docker run --rm -p 8050:8050 katonic/apps:likelihood-loan-default-prediction
